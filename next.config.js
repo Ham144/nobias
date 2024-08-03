19534f313a9f57a9762b47deb0372ebd4d8b8b4c
ham144 <24434muhammad.yafizham@gmail.com>
2024-08-03 22:47:28 +0700

    wdw

--- next.config.js
+++ next.config.js
@@ -1,15 +1,25 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+// next.config.js
 
-  swcMinify: true,
+module.exports = {
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    // Only include static pages in the export
+    return {
+      '/': { page: '/' },
+      // List other static pages here
+      // Exclude the /login page to avoid static export
+
+
+    };
+
+  },
   images: {
     remotePatterns: [
       {
         hostname: "lh3.googleusercontent.com"
       }
     ]
-  }
-}
-
-module.exports = nextConfig
+  },
+};
--- pages/api/auth/[...nextAuth].ts
+++ pages/api/auth/[...nextAuth].ts
@@ -1,22 +1,24 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-export default NextAuth({
+export const authOptions = {
+	pages: {
+		signIn: "/login",
+	},
 	providers: [
 		GoogleProvider({
-			clientId: process.env.GOOGLE_CLIENT_ID as string,
-			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+			clientId: String(process.env.GOOGLE_ID),
+			clientSecret: String(process.env.GOOGLE_SECRET),
 		}),
 	],
 	secret: process.env.NEXTAUTH_SECRET,
 	callbacks: {
 		async session({ session, token, user }: any) {
-			session.user.id = token?.sub;
+			// Add custom logic if needed
+			session.user.id = token.sub;
 			return session;
 		},
 	},
-	pages: {
-		signIn: "/auth/signin",
-	},
-	debug: true, // Enable debug mode
-});
+};
+
+export default NextAuth(authOptions);
